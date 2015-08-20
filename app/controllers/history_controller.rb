9347acb67813d4e361abbca2816e5b0b03d76af2
patitonav <pato.lalala@gmail.com>
2015-08-20 17:22:53 -0300

    Edicion de datos de usuario

--- app/controllers/history_controller.rb
+++ app/controllers/history_controller.rb
@@ -1,4 +1,5 @@
 class HistoryController < ApplicationController
+	before_action :authenticate_user!
   def index
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
   root to: 'dashboard#index'
   resources :diets
   
