27ae6a9b6ff51b51b79ba892f45e4e9862fb2a68
patitonav <pato.lalala@gmail.com>
2015-08-20 17:39:26 -0300

    Se agrego los datos de obra social al editar usuario

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,6 +3,6 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def account_update_params
-    params.require(:user).permit(:name, :last_name, :address, :city, :country, :phone, :cellphone, :email, :password, :password_confirmation, :current_password)
+    params.require(:user).permit(:name, :last_name, :address, :city, :country, :phone, :cellphone, :social_security, :social_security_plan, :social_security_number, :email, :password, :password_confirmation, :current_password)
   end
 end
