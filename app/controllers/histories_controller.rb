6f0659c32dd44dee9f311b29a4b39c92c611087d
patitonav <pato.lalala@gmail.com>
2015-09-06 15:52:49 -0300

    Arreglando Histories

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,11 +1,16 @@
 class HistoryController < ApplicationController
 	before_action :authenticate_user!
+	
   def index
   end
 
   def create
-    @history = History.build(history_params)
+    @history = History.new(history_params)
     @history.save
+  end
+
+  def new
+  	@history =History.new
   end
   
   private 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'history/index'
 
-  get 'history/create'
 
   get 'static_pages/help'
-
+  resources :histories do 
+  end
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users, :controllers => { registrations: 'registrations' }
