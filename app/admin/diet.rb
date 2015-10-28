0872adedf01c2339f5ac921e55eb6ab99569b594
ma-alvarez <magual89@gmail.com>
2015-10-28 16:56:44 -0300

    adding edition from active admin diet view

--- app/admin/diet.rb
+++ app/admin/diet.rb
@@ -1,5 +1,6 @@
-ActiveAdmin.register User, as: "Dietas" do
-  config.clear_action_items!
+ActiveAdmin.register User, as: "Dieta" do
+  # config.clear_action_items!
+  actions :index, :show, :update, :edit
   controller do
     def permitted_params
       params.permit! # allow all parameters
@@ -16,7 +17,7 @@ ActiveAdmin.register User, as: "Dietas" do
 
  show do
     panel "Detalles de la Dieta" do
-      attributes_table_for dietas.diet do
+      attributes_table_for dieta.diet do
         row :fecal_incontinence
         row :hemacromotosis
         row :celiac
@@ -30,7 +31,7 @@ ActiveAdmin.register User, as: "Dietas" do
     end
 
     panel "Desayuno" do
-      attributes_table_for dietas.diet do
+      attributes_table_for dieta.diet do
         row :breakfast_fruits
         row :breakfast_lacteals
         row :breakfast_starch
@@ -38,7 +39,7 @@ ActiveAdmin.register User, as: "Dietas" do
     end
 
     panel "Colación" do
-      attributes_table_for dietas.diet do
+      attributes_table_for dieta.diet do
         row :collation_fruits
         row :collation_lacteals
         row :collation_starch
@@ -46,7 +47,7 @@ ActiveAdmin.register User, as: "Dietas" do
     end
 
     panel "Almuerzo" do
-      attributes_table_for dietas.diet do
+      attributes_table_for dieta.diet do
         row :lunch_starch
         row :lunch_vegetables
         row :luch_fats
@@ -57,7 +58,7 @@ ActiveAdmin.register User, as: "Dietas" do
     end
 
     panel "Merienda" do
-      attributes_table_for dietas.diet do
+      attributes_table_for dieta.diet do
         row :merienda_fruits
         row :merienda_lacteals
         row :merienda_starch
@@ -65,7 +66,7 @@ ActiveAdmin.register User, as: "Dietas" do
     end
 
     panel "Cena" do
-      attributes_table_for dietas.diet do
+      attributes_table_for dieta.diet do
         row :dinner_starch
         row :dinner_vegetables
         row :dinner_fats
@@ -75,6 +76,70 @@ ActiveAdmin.register User, as: "Dietas" do
      end
     end
 
+  end
+
+  form do |f|
+    f.inputs "Detalles de la Dieta" do
+      f.semantic_fields_for :diet do |d|  
+        d.input :fecal_incontinence
+        d.input :hemacromotosis
+        d.input :celiac
+        d.input :irritable_colon
+        d.input :pregnancy
+        d.input :ovolact
+        d.input :kosher
+        d.input :without_fibers
+        d.input :allowed_foods
+      end
+    end
+
+    f.inputs "Desayuno" do
+      f.semantic_fields_for :diet do |d|
+        d.input :breakfast_fruits
+        d.input :breakfast_lacteals
+        d.input :breakfast_starch
+      end
+    end
+
+    f.inputs "Colación" do
+      f.semantic_fields_for :diet do |d|
+        d.input :collation_fruits
+        d.input :collation_lacteals
+        d.input :collation_starch
+      end
+    end
+
+    f.inputs "Almuerzo" do
+      f.semantic_fields_for :diet do |d|
+        d.input :lunch_starch
+        d.input :lunch_vegetables
+        d.input :luch_fats
+        d.input :lunch_fruits
+        d.input :lunch_lacteals
+        d.input :lunch_meats
+      end
+    end
+
+    f.inputs "Merienda" do
+      f.semantic_fields_for :diet do |d|
+        d.input :merienda_fruits
+        d.input :merienda_lacteals
+        d.input :merienda_starch
+      end
+    end
+
+    f.inputs "Cena" do
+      f.semantic_fields_for :diet do |d|
+        d.input :dinner_starch
+        d.input :dinner_vegetables
+        d.input :dinner_fats
+        d.input :dinner_fruits
+        d.input :dinner_lacteals
+        d.input :dinner_meats
+      end
+    end
+
+    f.actions
   end
 
   filter :name
