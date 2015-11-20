3f986ea3b6f1e511a637139fcaf58b19753d69bd
patitonav <pato.lalala@gmail.com>
2015-11-20 18:20:43 -0300

    Preparando vista y controller para manejar carga de medidas

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -14,13 +14,19 @@ class HistoriesController < ApplicationController
   end
 
   def new
+    @open = is_open
   	@history = current_user.histories.new
   end
+
   
+
   private 
 
   def history_params
   	params.require(:history).permit(:weight, :waist, :hip, :leg, :fat, :user_id, :date)
   end
-
+  #Check for open record
+  def is_open
+      return false
+  end
 end
