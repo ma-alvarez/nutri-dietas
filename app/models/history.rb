74ee73690653dcb00a76838c43f30b24943f2a78
ma-alvarez <magual89@gmail.com>
2015-11-30 12:22:53 -0300

    adding fats_value and risk

--- app/models/history.rb
+++ app/models/history.rb
@@ -5,5 +5,33 @@ class History < ActiveRecord::Base
 		index = self.weight / (self.user.height * self.user.height)
     	index.round(2)
 	end
+
+	def risk
+		if self.user.sex == 'M'
+			if self.waist < 94
+				return "Bajo"
+			end
+			if self.waist >= 94 && self.waist < 102
+				return "Medio"
+			end
+			if self.waist >= 102
+			 	return "Alto"
+			end 	
+		else
+			if self.waist < 80
+				return "Bajo"
+			end
+			if self.waist >= 80 && self.waist < 88
+				return "Medio"
+			end
+			if self.waist >= 88
+				return "Alto"
+			end
+		end
+	end
+
+	def fats_value
+		((self.fat * self.weight)/100).round(2)
+	end
 	
 end
