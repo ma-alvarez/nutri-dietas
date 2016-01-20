16ee7648bb91691102c1fc627f429ae27e7cf7bf
patitonav <pato.lalala@gmail.com>
2016-01-19 21:15:40 -0300

    Se agrego la confirmacion basica

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160118185533) do
+ActiveRecord::Schema.define(version: 20160119232735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -227,8 +227,12 @@ ActiveRecord::Schema.define(version: 20160118185533) do
     t.string   "how_meet"
     t.text     "comments"
     t.boolean  "intestinal_diseases"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
